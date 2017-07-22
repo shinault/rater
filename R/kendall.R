52066c9a7fc45527f73c5897a6bc7bb44a73077c
GMS <gshinault@gmail.com>
2017-07-21 22:31:21 -0500

    Complete node positioning for ranking comparison plots

--- R/kendall.R
+++ R/kendall.R
@@ -27,6 +27,58 @@ kendall <- function(x, y, unranked_items=NULL) {
   (2*n_conc - choose(n_items, 2)) / (n_conc + n_disc)
 }
 
+unranked <- function(x, y, other_rankings = NULL) {
+  all_items <- unique(c(x, y, other_rankings))
+  list(setdiff(all_items, x), setdiff(all_items,y))
+}
+
+node_labels <- function(x, y, other_rankings = NULL) {
+  labs <- x
+  n_unranked <- purrr::map(unranked(x, y, other_rankings), length)
+  if (n_unranked[1] > 0) {
+    labs <- append(labs, paste(n_unranked[1], "unranked"))
+  }
+  labs <- append(labs, y)
+  if (n_unranked[2] > 0) {
+    labs <- append(labs, paste(n_unranked[2], "unranked"))
+  }
+  labs
+}
+
+node_pos <- function(x, y, other_rankings = NULL) {
+  gap <- 0.5
+  height <- 2*max(length(x), length(y))
+  xpos <- rep(0, times = length(x))
+  ypos <- seq(from = height, by = -gap, length.out = length(x))
+  n_unranked <- purrr::map(unranked(x, y, other_rankings), length)
+  if (n_unranked[1] > 0) {
+    xpos <- append(xpos, 0)
+    ypos <- append(ypos, tail(ypos, 1)-gap)
+  }
+  xpos <- append(xpos, rep(2, times = length(y)))
+  ypos <- append(ypos, seq(from = height, by = -gap, length.out = length(y)))
+  if (n_unranked[2] > 0) {
+    xpos <- append(xpos, 2)
+    ypos <- append(ypos, tail(ypos, 1)-gap)
+  }
+  list(xpos, ypos)
+}
+
+fighter_nodes <- function(x, y, other_rankings = NULL) {
+  labs <- node_labels(x, y, other_rankings)
+  pos <- node_pos(x, y, other_rankings)
+  n_nodes <- length(labs)
+  DiagrammeR::create_node_df(
+    n = n_nodes,
+    type = "item",
+    nodes = 1:n_nodes,
+    label = labs,
+    x = pos[[1]],
+    y = pos[[2]],
+    shape = "rectangle"
+  )
+}
+
 #' Auxiliary function.
 zipit <- function(x, y) {
   allpairs <- purrr::map(x, function(xi) purrr::map(y, function(yi) c(xi, yi)))
